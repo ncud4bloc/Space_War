1bc84815f056bbf79a508303fc931143b609128d
Neil Cudden <ncudden@sbcglobal.net>
2020-10-07 15:12:17 -0700

    CSS cleaned up and screen size set to 900 x 930 for now

--- JS/scriptMC - Copy.js
+++ JS/scriptMC - Copy.js
@@ -8,7 +8,31 @@ var alienShip;
 function startGame(){
     gameArea.start();
     earthShip = new Spaceship(100,300,80,295,60,290,70,300,60,310,80,305,100,300,"#15eb46","miter");
-    alienShip = new Spaceship(660,300,680,298,700,290,700,300,700,310,680,302,660,300,"#15eb46","round");
+    alienShip = new Spaceship(760,630,780,628,800,620,805,630,800,640,780,632,760,630,"#15eb46","round");
+}
+
+function updateGameArea(){
+    gameArea.clear();
+    
+    // Temporary translations to confirm it works
+    earthShip.x1 += 1;
+    earthShip.x2 += 1;
+    earthShip.x3 += 1;
+    earthShip.x4 += 1;
+    earthShip.x5 += 1;
+    earthShip.x6 += 1;
+    earthShip.x7 += 1;
+    alienShip.x1 -= 1;
+    alienShip.x2 -= 1;
+    alienShip.x3 -= 1;
+    alienShip.x4 -= 1;
+    alienShip.x5 -= 1;
+    alienShip.x6 -= 1;
+    alienShip.x7 -= 1;
+    // End Temporary translations
+    
+    earthShip.update();
+    alienShip.update();
 }
 
 function Spaceship(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y,p6x,p6y,p7x,p7y,color,joint){
@@ -43,46 +67,20 @@ function Spaceship(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y,p6x,p6y,p7x,p7y,color
     }   
 }
 
-function updateGameArea(){
-    gameArea.clear();
-    
-    // Temporary translations to confirm it works
-    earthShip.x1 += 1;
-    earthShip.x2 += 1;
-    earthShip.x3 += 1;
-    earthShip.x4 += 1;
-    earthShip.x5 += 1;
-    earthShip.x6 += 1;
-    earthShip.x7 += 1;
-    alienShip.x1 -= 1;
-    alienShip.x2 -= 1;
-    alienShip.x3 -= 1;
-    alienShip.x4 -= 1;
-    alienShip.x5 -= 1;
-    alienShip.x6 -= 1;
-    alienShip.x7 -= 1;
-    // End Temporary translations
-    
-    earthShip.update();
-    alienShip.update();
-}
-
-
-
 /* ---------------   Objects   --------------- */
 
 var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-      this.canvas.width = 800;
-      this.canvas.height = 600;
+      this.canvas.width = 900;
+      this.canvas.height = 930;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.interval = setInterval(updateGameArea,20);
+      this.interval = setInterval(updateGameArea,30);
     },
     clear : function(){
-      this.canvas.width = 800;
-      this.canvas.height = 600;
+      this.canvas.width = 900;
+      this.canvas.height = 930;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
